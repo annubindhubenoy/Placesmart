c2772d8f238299047ee4ad51e327513f533c4e5a
annubindhubenoy <annubindhubinoy@gmail.com>
2025-09-14 07:55:21 +0530

    Create models.py

---
+++ user/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class CustomUser(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)  # store hashed passwords
+
+    def __str__(self):
+        return self.email
+
+# Create your models here.
